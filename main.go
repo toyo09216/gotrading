87cc4e7ee9363ac8f17b92d8640f788552f792f5
toyo09216 <atsushi09216@gmail.com>
2023-11-25 10:17:54 +0900

    [add] API認証の実装

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"gotrading/bitflyer"
 	"gotrading/config"
 	"gotrading/utils"
-	"log"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	fmt.Println(apiClient.GetBalance())
 }
