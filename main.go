867daabbd8ca85abeff7e40d43cb807442c04a81
toyo09216 <atsushi09216@gmail.com>
2023-11-25 18:21:36 +0900

    [add] データベーススキーマを作成

--- main.go
+++ main.go
@@ -2,32 +2,12 @@ package main
 
 import (
 	"fmt"
-	"gotrading/bitflyer"
+	"gotrading/app/models"
 	"gotrading/config"
 	"gotrading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-
-	order := &bitflyer.Order{
-		ProductCode:     config.Config.ProductCode,
-		ChildOrderType:  "MARKET",
-		Side:            "BUY",
-		Size:            0.001,
-		MinuteToExpires: 1,
-		TimeInForce:     "GTC",
-	}
-	res, _ := apiClient.SendOrder(order)
-	fmt.Println(res.ChildOrderAcceptanceID)
-
-	//i := "JRF20181012-144016-140584"
-	//params := map[string]string{
-	//	"product_code": config.Config.ProductCode,
-	//	"child_order_acceptance_id": i,
-	//}
-	//r, _ := apiClient.ListOrder(params)
-	//fmt.Println(r)
-
+	fmt.Println(models.DbConnection)
 }
