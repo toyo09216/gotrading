dd65069ecd59a59f9df89bc9b41683ea1bc2afb2
toyo09216 <atsushi09216@gmail.com>
2023-11-25 19:53:24 +0900

    [add] webサーバーを立ち上げて、GoogleCandleチャートを表示

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"gotrading/app/controllers"
-	"gotrading/app/models"
 	"gotrading/config"
 	"gotrading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }
