dd8d46e4e8a34c8471cb9cde1f4755877598fa4e
toyo09216 <atsushi09216@gmail.com>
2023-12-03 17:37:17 +0900

    [add] インディケーターのランキング機能を実装

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"log"
-
 	"gotrading/app/controllers"
 	"gotrading/config"
 	"gotrading/utils"
+	"log"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	controllers.StreamIngestionData()
-	controllers.StartWebServer()
 	log.Println(controllers.StartWebServer())
 }
