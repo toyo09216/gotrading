9a036c9fb126a81b29a90d4d0eaf49a497e94b4c
toyo09216 <atsushi09216@gmail.com>
2023-12-03 16:12:37 +0900

    [add] イベントの表示機能を実装

--- main.go
+++ main.go
@@ -1,32 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"gotrading/app/models"
-	"time"
+	"log"
+
+	"gotrading/app/controllers"
+	"gotrading/config"
+	"gotrading/utils"
 )
 
-//import (
-//	"gotrading/app/controllers"
-//	"gotrading/config"
-//	"gotrading/utils"
-//)
-//
-//func main(){
-//	utils.LoggingSettings(config.Config.LogFile)
-//	controllers.StreamIngestionData()
-//	controllers.StartWebServer()
-//}
-
 func main() {
-	s := models.NewSignalEvents()
-	df, _ := models.GetAllCandle("BTC_JPY", time.Minute, 10)
-	c1 := df.Candles[0]
-	c2 := df.Candles[5]
-	s.Buy("BTC_JPY", c1.Time.UTC(), c1.Close, 1.0, true)
-	s.Sell("BTC_JPY", c2.Time.UTC(), c2.Close, 1.0, true)
-	fmt.Println(models.GetSignalEventsByCount(1))
-	fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
-	fmt.Println(s.CollectAfter(time.Now().UTC()))
-	fmt.Println(s.CollectAfter(c1.Time))
+	utils.LoggingSettings(config.Config.LogFile)
+	controllers.StreamIngestionData()
+	controllers.StartWebServer()
+	log.Println(controllers.StartWebServer())
 }
