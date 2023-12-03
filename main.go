c6727b4bd2f776da1fc8d0439adfd6a5b2b946ba
toyo09216 <atsushi09216@gmail.com>
2023-12-03 14:59:12 +0900

    [add] イベントモデルの作成

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"gotrading/app/controllers"
-	"gotrading/config"
-	"gotrading/utils"
+	"fmt"
+	"gotrading/app/models"
+	"time"
 )
 
+//import (
+//	"gotrading/app/controllers"
+//	"gotrading/config"
+//	"gotrading/utils"
+//)
+//
+//func main(){
+//	utils.LoggingSettings(config.Config.LogFile)
+//	controllers.StreamIngestionData()
+//	controllers.StartWebServer()
+//}
+
 func main() {
-	utils.LoggingSettings(config.Config.LogFile)
-	controllers.StreamIngestionData()
-	controllers.StartWebServer()
+	s := models.NewSignalEvents()
+	df, _ := models.GetAllCandle("BTC_JPY", time.Minute, 10)
+	c1 := df.Candles[0]
+	c2 := df.Candles[5]
+	s.Buy("BTC_JPY", c1.Time.UTC(), c1.Close, 1.0, true)
+	s.Sell("BTC_JPY", c2.Time.UTC(), c2.Close, 1.0, true)
+	fmt.Println(models.GetSignalEventsByCount(1))
+	fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
+	fmt.Println(s.CollectAfter(time.Now().UTC()))
+	fmt.Println(s.CollectAfter(c1.Time))
 }
